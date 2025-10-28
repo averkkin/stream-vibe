16587a8da0505ef38cc5523c2d92cf555ea85e08
Daniil Averkin <info@averkkin.ru>
2025-10-28 23:01:19 +0300

    Added two new components: Button, Icon

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,11 +1,65 @@
 import './Header.scss'
 import Logo from '@/components/Logo'
+import Button from '@/components/Button'
+import classNames from "classnames";
 
-const Header = () => {
+const Header = (props) => {
+    const { url } = props;
+
+    const menuItems = [
+        {
+            label: 'Home',
+            href: '/'
+        },
+        {
+            label: 'Movies & Shows',
+            href: '/movies'
+        },
+        {
+            label: 'Support',
+            href: '/support'
+        },
+        {
+            label: 'Subscriptions',
+            href: '/subscriptions'
+        },
+    ]
+
     return (
         <header className="header">
             <div className="header__inner container">
                 <Logo className="header__logo" loading="eager" />
+                <nav className="header__menu">
+                    <ul className="header__menu-list">
+                        {menuItems.map(({label, href}, index) => (
+                            <li key={index} className="header__menu-item" >
+                                <a
+                                    className={classNames('header__menu-link', {
+                                        'is-active': href === url,
+                                    })}
+                                    href={href}>
+                                    { label }
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="header__actions">
+                    <Button
+                        className="header__button"
+                        label="Search"
+                        isLabelHidden
+                        iconName="search"
+                        mode='transparent'
+                    />
+                    <Button
+                        href="/"
+                        label="Notification"
+                        isLabelHidden
+                        iconName="notify"
+                        mode='transparent'
+                    />
+                </div>
             </div>
         </header>
     )
